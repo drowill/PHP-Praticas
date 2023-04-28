8153ab06005c097f4638e86cd9677e7876496bb8
pedro! <100161312+drowill@users.noreply.github.com>
2023-04-28 10:54:11 -0300

    Add files via upload

---
+++ AtividadeDupla/showpage.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ShowPage</title>
+</head>
+<body>
+    <h1>ShowPage</h1>
+    <h2>Escolha os numeros</h2>
+    <form action="result.php" method="post">
+        <label for="num01">Numero 01:</label>
+        <input type="text" name="num01">
+        <label for="num02">Numero 02:</label>
+        <input type="text" name="num02">
+        <button>Enviar</button>
+    </form>
+</body>
+</html>
